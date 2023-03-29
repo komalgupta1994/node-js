ea192fa0f062187620512826ac25cb0508838c05
Lovelesh <lovelesh@gmail.com>
2023-03-29 17:58:10 +0530

    add nodemon and package json

--- First Node js/routes.js
+++ First Node js/routes.js
@@ -7,7 +7,7 @@ const requestHandler = (req, res) => {
     if (url == '/') {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
-        res.write('<head>Enter your value</head>');
+        res.write('<head>Enter your new value</head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></body>');
         res.write('</html>');
         return res.end();
