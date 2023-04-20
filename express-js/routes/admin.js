fcfa5b1de399725934dcf0ac06e05c6654a89ad3
komalgupta1994 <komgupta1@WKMZTA0ADD4F.global.publicisgroupe.net>
2023-04-20 14:18:55 +0530

    add routing and static files

--- express-js/routes/admin.js
+++ express-js/routes/admin.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
+const rootDir = require('../utils/path');
 
 const router = express.Router();
 
 // use method is call on every incoming request
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 })
 
 router.post('/product', (req, res, next) => {
--- express-js/server.js
+++ express-js/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -8,12 +9,16 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
+// this is use for expose the static/public files/folder to outside of world
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.use('/admin', adminRoutes);
 
 app.use(shopRoutes);
 
 app.use('/', (req, res) => {
-    res.send('<h1>Page Not Found</h1>')
+    // res.send('<h1>Page Not Found</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 app.listen(3001);
